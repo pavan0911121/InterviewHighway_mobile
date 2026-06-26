72d6babda87a77cdf1791c52a39d419cca963c52
pavan0911121 <pavankarthik0911@gmail.com>
2026-06-26 11:59:30 +0530

    android commit works fine in anrdoid an ios

--- android/app/.cxx/Debug/4v26o526/arm64-v8a/CMakeFiles/VerifyGlobs.cmake
+++ android/app/.cxx/Debug/4v26o526/arm64-v8a/CMakeFiles/VerifyGlobs.cmake
@@ -158,6 +158,30 @@ if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   file(TOUCH_NOCREATE "/Users/pavankarthik/InterviewHighway_mobile/android/app/.cxx/Debug/4v26o526/arm64-v8a/CMakeFiles/cmake.verify_globs")
 endif()
 
+# react_codegen_SRCS at /Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-document-picker/android/build/generated/source/codegen/jni/CMakeLists.txt:9 (file)
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-document-picker/android/build/generated/source/codegen/jni/*.cpp")
+set(OLD_GLOB
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-document-picker/android/build/generated/source/codegen/jni/rndocumentpicker-generated.cpp"
+  )
+if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
+  message("-- GLOB mismatch!")
+  file(TOUCH_NOCREATE "/Users/pavankarthik/InterviewHighway_mobile/android/app/.cxx/Debug/4v26o526/arm64-v8a/CMakeFiles/cmake.verify_globs")
+endif()
+
+# react_codegen_SRCS at /Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-document-picker/android/build/generated/source/codegen/jni/CMakeLists.txt:9 (file)
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-document-picker/android/build/generated/source/codegen/jni/react/renderer/components/rndocumentpicker/*.cpp")
+set(OLD_GLOB
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-document-picker/android/build/generated/source/codegen/jni/react/renderer/components/rndocumentpicker/ComponentDescriptors.cpp"
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-document-picker/android/build/generated/source/codegen/jni/react/renderer/components/rndocumentpicker/EventEmitters.cpp"
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-document-picker/android/build/generated/source/codegen/jni/react/renderer/components/rndocumentpicker/Props.cpp"
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-document-picker/android/build/generated/source/codegen/jni/react/renderer/components/rndocumentpicker/ShadowNodes.cpp"
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-document-picker/android/build/generated/source/codegen/jni/react/renderer/components/rndocumentpicker/States.cpp"
+  )
+if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
+  message("-- GLOB mismatch!")
+  file(TOUCH_NOCREATE "/Users/pavankarthik/InterviewHighway_mobile/android/app/.cxx/Debug/4v26o526/arm64-v8a/CMakeFiles/cmake.verify_globs")
+endif()
+
 # react_codegen_SRCS at /Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-gesture-handler/android/build/generated/source/codegen/jni/CMakeLists.txt:9 (file)
 file(GLOB NEW_GLOB LIST_DIRECTORIES true "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-gesture-handler/android/build/generated/source/codegen/jni/*.cpp")
 set(OLD_GLOB
@@ -176,6 +200,30 @@ set(OLD_GLOB
   "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-gesture-handler/android/build/generated/source/codegen/jni/react/renderer/components/rngesturehandler_codegen/Props.cpp"
   "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-gesture-handler/android/build/generated/source/codegen/jni/react/renderer/components/rngesturehandler_codegen/ShadowNodes.cpp"
   "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-gesture-handler/android/build/generated/source/codegen/jni/react/renderer/components/rngesturehandler_codegen/States.cpp"
+  )
+if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
+  message("-- GLOB mismatch!")
+  file(TOUCH_NOCREATE "/Users/pavankarthik/InterviewHighway_mobile/android/app/.cxx/Debug/4v26o526/arm64-v8a/CMakeFiles/cmake.verify_globs")
+endif()
+
+# react_codegen_SRCS at /Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-razorpay/android/build/generated/source/codegen/jni/CMakeLists.txt:9 (file)
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-razorpay/android/build/generated/source/codegen/jni/*.cpp")
+set(OLD_GLOB
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-razorpay/android/build/generated/source/codegen/jni/RNRazorpayCheckoutSpec-generated.cpp"
+  )
+if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
+  message("-- GLOB mismatch!")
+  file(TOUCH_NOCREATE "/Users/pavankarthik/InterviewHighway_mobile/android/app/.cxx/Debug/4v26o526/arm64-v8a/CMakeFiles/cmake.verify_globs")
+endif()
+
+# react_codegen_SRCS at /Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-razorpay/android/build/generated/source/codegen/jni/CMakeLists.txt:9 (file)
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-razorpay/android/build/generated/source/codegen/jni/react/renderer/components/RNRazorpayCheckoutSpec/*.cpp")
+set(OLD_GLOB
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-razorpay/android/build/generated/source/codegen/jni/react/renderer/components/RNRazorpayCheckoutSpec/ComponentDescriptors.cpp"
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-razorpay/android/build/generated/source/codegen/jni/react/renderer/components/RNRazorpayCheckoutSpec/EventEmitters.cpp"
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-razorpay/android/build/generated/source/codegen/jni/react/renderer/components/RNRazorpayCheckoutSpec/Props.cpp"
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-razorpay/android/build/generated/source/codegen/jni/react/renderer/components/RNRazorpayCheckoutSpec/ShadowNodes.cpp"
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-razorpay/android/build/generated/source/codegen/jni/react/renderer/components/RNRazorpayCheckoutSpec/States.cpp"
   )
 if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   message("-- GLOB mismatch!")
