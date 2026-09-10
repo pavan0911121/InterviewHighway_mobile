2366ea897ba2d0c93afa9340b3bf30800b3203c9
pavan0911121 <pavankarthik0911@gmail.com>
2026-09-10 09:51:11 +0530

    issues resolved

--- android/app/.cxx/Debug/4v26o526/x86/CMakeFiles/VerifyGlobs.cmake
+++ android/app/.cxx/Debug/4v26o526/x86/CMakeFiles/VerifyGlobs.cmake
@@ -206,6 +206,30 @@ if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   file(TOUCH_NOCREATE "/Users/pavankarthik/InterviewHighway_mobile/android/app/.cxx/Debug/4v26o526/x86/CMakeFiles/cmake.verify_globs")
 endif()
 
+# react_codegen_SRCS at /Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-image-picker/android/build/generated/source/codegen/jni/CMakeLists.txt:9 (file)
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-image-picker/android/build/generated/source/codegen/jni/*.cpp")
+set(OLD_GLOB
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-image-picker/android/build/generated/source/codegen/jni/RNImagePickerSpec-generated.cpp"
+  )
+if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
+  message("-- GLOB mismatch!")
+  file(TOUCH_NOCREATE "/Users/pavankarthik/InterviewHighway_mobile/android/app/.cxx/Debug/4v26o526/x86/CMakeFiles/cmake.verify_globs")
+endif()
+
+# react_codegen_SRCS at /Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-image-picker/android/build/generated/source/codegen/jni/CMakeLists.txt:9 (file)
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-image-picker/android/build/generated/source/codegen/jni/react/renderer/components/RNImagePickerSpec/*.cpp")
+set(OLD_GLOB
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-image-picker/android/build/generated/source/codegen/jni/react/renderer/components/RNImagePickerSpec/ComponentDescriptors.cpp"
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-image-picker/android/build/generated/source/codegen/jni/react/renderer/components/RNImagePickerSpec/EventEmitters.cpp"
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-image-picker/android/build/generated/source/codegen/jni/react/renderer/components/RNImagePickerSpec/Props.cpp"
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-image-picker/android/build/generated/source/codegen/jni/react/renderer/components/RNImagePickerSpec/ShadowNodes.cpp"
+  "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-image-picker/android/build/generated/source/codegen/jni/react/renderer/components/RNImagePickerSpec/States.cpp"
+  )
+if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
+  message("-- GLOB mismatch!")
+  file(TOUCH_NOCREATE "/Users/pavankarthik/InterviewHighway_mobile/android/app/.cxx/Debug/4v26o526/x86/CMakeFiles/cmake.verify_globs")
+endif()
+
 # react_codegen_SRCS at /Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-razorpay/android/build/generated/source/codegen/jni/CMakeLists.txt:9 (file)
 file(GLOB NEW_GLOB LIST_DIRECTORIES true "/Users/pavankarthik/InterviewHighway_mobile/node_modules/react-native-razorpay/android/build/generated/source/codegen/jni/*.cpp")
 set(OLD_GLOB
